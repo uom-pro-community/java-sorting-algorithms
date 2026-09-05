f4e221d4113f4bb1401bee959424c4ae6364d08d
HarithaMihimal <harithamihimal4@gmail.com>
2026-09-06 03:43:46 +0530

    Add ArraysSortExample implementation using Java's built-in sorting

---
+++ 06-java-built-in-sorting/ArraysSortExample.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class ArraysSortExample {
+    public static void main(String[] args) {
+        int[] values = { 5, 1, 4, 2, 8 };
+        Arrays.sort(values);
+        System.out.println(Arrays.toString(values));
+    }
+}
